40cdc881ad2ac881bfb968fdbe2b0fa48c20bfda
Riddhi Chaudhari <mukul4u@gmail.com>
2022-03-04 11:55:06 +0530

    Reverted Mapper changes

--- src/main/java/com/mukul/utility/mapper/examples/EmployeeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mukul.utility.mapper.examples;
-
-import com.mukul.utility.mapper.DefaultMapperConfig;
-import org.mapstruct.Mapper;
-
-@Mapper(config = DefaultMapperConfig.class)
-public interface EmployeeMapper extends MapperBase<EmployeeEntity,EmployeeModel> {
-
-}
--- src/main/java/com/mukul/utility/mapper/examples/MapperBase.java
+++
@@ -1,8 +0,0 @@
-package com.mukul.utility.mapper.examples;
-
-public interface MapperBase<E,M> {
-    M entityToModel (E entity);
-
-    E modelToEntity(M model);
-}
-
